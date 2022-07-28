2ad23ca00affabf99d27790d0b4677f6090b9182
asceznyk <asceznyk@gmail.com>
2022-07-28 22:31:59 +0530

    just wrote some more crap with some more shit

--- nst.py
+++ nst.py
@@ -53,9 +53,9 @@ style_image = load_image(sys.argv[2])
 gen_image = base_image.clone().requires_grad_(True)
 
 total_steps = 6000
-learning_rate = 1e-3
+learning_rate = 1e-2
 alpha = 1
-beta = 1000
+beta = 100000
 optimizer = optim.Adam([gen_image], lr=learning_rate)
 
 model.to(device)
