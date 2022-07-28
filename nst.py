4fa8d989ccd831dd86e3fc619c6085deae158063
asceznyk <asceznyk@gmail.com>
2022-07-28 21:05:23 +0530

    just wrote some crap

--- nst.py
+++ nst.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.models as models
 
-from torchvision import save_image
+from torchvision.utils import save_image
 
 from PIL import Image
 
