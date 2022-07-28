1fec58bbb3dce18640f33151c42cce98c36b5a9c
asceznyk <asceznyk@gmail.com>
2022-07-28 21:11:02 +0530

    just wrote some more crap

--- nst.py
+++ nst.py
@@ -8,7 +8,24 @@ from torchvision.utils import save_image
 
 from PIL import Image
 
+def load_image(image_name):
+    image = Image.open(image_name)
+    image = loader(image).unsqueeze(0)
+    return image.to(device)
+
 model = models.vgg19(pretrained=True).features
 print(model)
+
+device = torch.device("cuda" if torch.cuda.is_available else "cpu")
+image_size = 356
+
+loader = transforms.Compose([
+    transforms.Resize((image_size, image_size)),
+    transforms.ToTensor()
+])
+
+base_image = load_image('images/aizen_prof.png')
+style_image = load_image('images/female_head_picasso.jpg')
+
 
 
