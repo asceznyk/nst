e2315f69a951f04807317d4777f9a5d8fff8a536
asceznyk <asceznyk@gmail.com>
2022-07-28 21:03:48 +0530

    just wrote some crap

---
+++ nst.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+import torchvision.transfroms as transforms
+import torchvision.models as models
+
+from torchvision import save_image
+
+from PIL import Image
+
+model = models.vgg19(pretrained=True).features
+
+
